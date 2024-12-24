ec12ad744ae5db636ee7a9bdb77a74e289861eb9
wkwk <hanifamar17@gmail.com>
2024-12-24 11:53:08 +0700

    fix: update color for warning message

--- app.py
+++ app.py
@@ -14,49 +14,49 @@ KUNJUNGAN_SHEET = 'Kunjungan'
 
 @app.route("/")
 def index():
-    return redirect(url_for("home"))
+    return redirect(url_for("home_submit"))
 
-@app.route("/home", methods=["POST", "GET"])
-def home():
-    if request.method == 'POST':
-        id_pengunjung = request.form.get('id_pengunjung')
-
-        try:
-            # Load workbook dan sheet
-            wb = load_workbook(EXCEL_FILE)
-            pengunjung_sheet = wb[PENGUNJUNG_SHEET]
-            kunjungan_sheet = wb[KUNJUNGAN_SHEET]
-
-            # Cari data pengunjung berdasarkan ID
-            pengunjung = None
-            for row in pengunjung_sheet.iter_rows(min_row=2, values_only=True):
-                if str(row[0]) == id_pengunjung:  # Kolom pertama adalah ID
-                    pengunjung = row
-                    break
-
-            if pengunjung:
-                # Rekam kunjungan
-                tanggal_kunjungan = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                kunjungan_sheet.append([pengunjung[0], pengunjung[1], pengunjung[2], tanggal_kunjungan])
-                wb.save(EXCEL_FILE)
-                #flash(f"Selamat datang <span class='font-bold'>{pengunjung[1]}</span>, selamat membaca!", 'success')
-                return jsonify({
-                'success': True,
-                'nama': pengunjung[1],
-                'redirect': url_for('home')
-            })
-            else:
-                #flash("ID pengunjung tidak terdaftar dalam sistem. Silakan hubungi petugas!", 'danger')
-                return jsonify({
-                'success': False,
-                'redirect': url_for('home')
-            })
-        except Exception as e:
-            flash(f"Terjadi kesalahan: {e}", 'danger')
-
-        return redirect(url_for('home'))
-
-    return render_template('index-scan.html')
+#@app.route("/home", methods=["POST", "GET"])
+#def home():
+#    if request.method == 'POST':
+#        id_pengunjung = request.form.get('id_pengunjung')
+#
+#        try:
+#            # Load workbook dan sheet
+#            wb = load_workbook(EXCEL_FILE)
+#            pengunjung_sheet = wb[PENGUNJUNG_SHEET]
+#            kunjungan_sheet = wb[KUNJUNGAN_SHEET]
+#
+#            # Cari data pengunjung berdasarkan ID
+#            pengunjung = None
+#            for row in pengunjung_sheet.iter_rows(min_row=2, values_only=True):
+#                if str(row[0]) == id_pengunjung:  # Kolom pertama adalah ID
+#                    pengunjung = row
+#                    break
+#
+#            if pengunjung:
+#                # Rekam kunjungan
+#                tanggal_kunjungan = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#                kunjungan_sheet.append([pengunjung[0], pengunjung[1], pengunjung[2], tanggal_kunjungan])
+#                wb.save(EXCEL_FILE)
+#                #flash(f"Selamat datang <span class='font-bold'>{pengunjung[1]}</span>, selamat membaca!", 'success')
+#                return jsonify({
+#                'success': True,
+#                'nama': pengunjung[1],
+#                'redirect': url_for('home')
+#            })
+#            else:
+#                #flash("ID pengunjung tidak terdaftar dalam sistem. Silakan hubungi petugas!", 'danger')
+#                return jsonify({
+#                'success': False,
+#                'redirect': url_for('home')
+#            })
+#        except Exception as e:
+#            flash(f"Terjadi kesalahan: {e}", 'danger')
+#
+#        return redirect(url_for('home'))
+#
+#    return render_template('index-scan.html')
 
 @app.route("/home/submit", methods=["POST", "GET"])
 def home_submit():
