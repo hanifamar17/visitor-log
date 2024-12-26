b5359c927cd89894ed784647e1e16c0a888197fd
wkwk <hanifamar17@gmail.com>
2024-12-26 08:59:16 +0700

    fix: updated the templates and fixed the route

--- app.bat
+++ app.bat
@@ -1,13 +1,13 @@
 @echo off
 
-REM Navigate to the buku_kunjung directory
-cd /d "E:\App-Development\buku_kunjung"
+REM Navigate to the visitor-log directory
+cd /d "E:\App-Development\visitor-log"
 
 REM Run the main.py script using Python
-start "" "python" "E:\App-Development\buku_kunjung\app.py"
+start "" "python" "E:\App-Development\visitor-log\app.py"
 
 REM Wait for main.py (adjust the timeout if necessary)
-timeout /t 3
+timeout /t 2
 
 REM Open the browser to http://127.0.0.1:5555
 start "" "http://127.0.0.1:5555"
--- app.py
+++ app.py
@@ -2,64 +2,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 from openpyxl import load_workbook
 from datetime import datetime
 
-
-
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 # Konfigurasi file Excel
-EXCEL_FILE = r'H:\My Drive\MANU\Buku-Kunjungan\buku_kunjung.xlsx'
-PENGUNJUNG_SHEET = 'Pengunjung'
-KUNJUNGAN_SHEET = 'Kunjungan'
+EXCEL_FILE = r'E:\App-Development\visitor-log\data\visitor-log.xlsx' #Direktori file excel yang digunakan untuk menyimpan data visitor
+PENGUNJUNG_SHEET = 'Pengunjung' #Nama sheet di file excel yang digunakan untuk menyimpan data visitor
+KUNJUNGAN_SHEET = 'Kunjungan' #Nama sheet di file excel yang digunakan untuk merekam data kunjungan visitor
 
 @app.route("/")
 def index():
-    return redirect(url_for("home_submit"))
+    return redirect(url_for("home"))
 
-#@app.route("/home", methods=["POST", "GET"])
-#def home():
-#    if request.method == 'POST':
-#        id_pengunjung = request.form.get('id_pengunjung')
-#
-#        try:
-#            # Load workbook dan sheet
-#            wb = load_workbook(EXCEL_FILE)
-#            pengunjung_sheet = wb[PENGUNJUNG_SHEET]
-#            kunjungan_sheet = wb[KUNJUNGAN_SHEET]
-#
-#            # Cari data pengunjung berdasarkan ID
-#            pengunjung = None
-#            for row in pengunjung_sheet.iter_rows(min_row=2, values_only=True):
-#                if str(row[0]) == id_pengunjung:  # Kolom pertama adalah ID
-#                    pengunjung = row
-#                    break
-#
-#            if pengunjung:
-#                # Rekam kunjungan
-#                tanggal_kunjungan = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-#                kunjungan_sheet.append([pengunjung[0], pengunjung[1], pengunjung[2], tanggal_kunjungan])
-#                wb.save(EXCEL_FILE)
-#                #flash(f"Selamat datang <span class='font-bold'>{pengunjung[1]}</span>, selamat membaca!", 'success')
-#                return jsonify({
-#                'success': True,
-#                'nama': pengunjung[1],
-#                'redirect': url_for('home')
-#            })
-#            else:
-#                #flash("ID pengunjung tidak terdaftar dalam sistem. Silakan hubungi petugas!", 'danger')
-#                return jsonify({
-#                'success': False,
-#                'redirect': url_for('home')
-#            })
-#        except Exception as e:
-#            flash(f"Terjadi kesalahan: {e}", 'danger')
-#
-#        return redirect(url_for('home'))
-#
-#    return render_template('index-scan.html')
-
-@app.route("/home/submit", methods=["POST", "GET"])
-def home_submit():
+@app.route("/visilog", methods=["POST", "GET"])
+def home():
     if request.method == 'POST':
         id_pengunjung = request.form.get('id_pengunjung')
 
@@ -78,27 +34,28 @@ def home_submit():
 
             if pengunjung:
                 # Rekam kunjungan
-                tanggal_kunjungan = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                kunjungan_sheet.append([pengunjung[0], pengunjung[1], pengunjung[2], tanggal_kunjungan])
+                TANGGAL_KUNJUNGAN = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                kunjungan_sheet.append([pengunjung[0], pengunjung[1], pengunjung[2], TANGGAL_KUNJUNGAN])
                 wb.save(EXCEL_FILE)
-                #flash(f"Selamat datang <span class='font-bold'>{pengunjung[1]}</span>, selamat membaca!", 'success')
+
                 return jsonify({
-                'success': True,
-                'nama': pengunjung[1],
-                'redirect': url_for('home_submit')
-            })
+                    'success': True,
+                    'nama': pengunjung[1],
+                    'redirect': url_for('home')
+                })
+
             else:
-                #flash("ID pengunjung tidak terdaftar dalam sistem. Silakan hubungi petugas!", 'danger')
                 return jsonify({
-                'success': False,
-                'redirect': url_for('home_submit')
-            })
+                    'success': False,
+                    'redirect': url_for('home')
+                })
+            
         except Exception as e:
             flash(f"Terjadi kesalahan: {e}", 'danger')
 
-        return redirect(url_for('home_submit'))
+        return redirect(url_for('home'))
 
-    return render_template('index-submit.html')
+    return render_template('visitor-log.html')
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0', port = 5555)
